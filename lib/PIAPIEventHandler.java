237efa4538b062d2f1c4f5c2ff102c7f00992d54
Clashsoft <clash2@hotmail.de>
2013-10-03 15:09:15 +0200

    Update checking

--- lib/PIAPIEventHandler.java
+++ lib/PIAPIEventHandler.java
@@ -1,8 +1,12 @@
 package com.chaosdev.playerinventoryapi.lib;
 
+import clashsoft.clashsoftapi.util.CSUpdate;
+import clashsoft.clashsoftapi.util.update.ModUpdate;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 
 public class PIAPIEventHandler
 {
@@ -19,5 +23,15 @@ public class PIAPIEventHandler
 	private void replaceInventory(EntityPlayer entityPlayer)
 	{
 		System.out.println("Replacing Player Inventory");
+	}
+	
+	@ForgeSubscribe
+	public void playerJoined(EntityJoinWorldEvent event)
+	{
+		if (event.entity instanceof EntityPlayer)
+		{
+			ModUpdate update = CSUpdate.checkForUpdate("Player Inventory API", "piapi", Reference.VERSION);
+			CSUpdate.notifyUpdate((EntityPlayer) event.entity, "Player Inventory API", update);
+		}
 	}
 }
--- lib/Reference.java
+++ lib/Reference.java
@@ -1,10 +1,13 @@
 package com.chaosdev.playerinventoryapi.lib;
 
+import clashsoft.clashsoftapi.util.CSUpdate;
+
 public class Reference
 {
 	public static final String	MOD_ID					= "PlayerInventoryAPI";
 	public static final String	MOD_NAME				= "Player Inventory API";
-	public static final String	VERSION					= "v1.5.2";
+	public static final int		REVISION				= 0;
+	public static final String	VERSION					= CSUpdate.CURRENT_VERSION + "-" + REVISION;
 	public static final String	COMMON_PROXY_LOCATION	= "com.chaosdev.playerinventoryapi.CommonProxy";
 	public static final String	CLIENT_PROXY_LOCATION	= "com.chaosdev.playerinventoryapi.ClientProxy";
 	public static final String	CHANNEL_NAME			= MOD_ID;
