e05eb3d687783e081fae797ed2fa77231b229922
Clashsoft <clashsoft@hotmail.com>
2014-06-29 13:24:11 +0200

    1.7.2-1.0.1

--- src/main/java/clashsoft/playerinventoryapi/PlayerInventoryAPI.java
+++ src/main/java/clashsoft/playerinventoryapi/PlayerInventoryAPI.java
@@ -21,7 +21,7 @@ public class PlayerInventoryAPI extends ClashsoftMod
 	public static final String			MODID					= "piapi";
 	public static final String			NAME					= "Player Inventory API";
 	public static final String			ACRONYM					= "piapi";
-	public static final String			VERSION					= CSUpdate.CURRENT_VERSION + "-1.0.0";
+	public static final String			VERSION					= CSUpdate.CURRENT_VERSION + "-1.0.1";
 	
 	@Instance(MODID)
 	public static PlayerInventoryAPI	instance;
